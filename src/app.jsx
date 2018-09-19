9b71cbb4f39c2899a437fbff7693fae8d123b0a3
Zach Massia <zmassia@gmail.com>
2018-09-19 16:54:36 -0400

    Use a unique electron-store filename instead of default

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Store from 'electron-store';
 
 import { Home } from './Components';
+import CONSTANTS from './constants';
 
-const store = new Store();
+const store = new Store({ name: CONSTANTS.STORE_FILENAME });
 
 export default class App extends Component {
   render() {
