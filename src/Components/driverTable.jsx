2de19717eb5694a62c1a1410c9d2d891f6baee9e
Zach Massia <zmassia@gmail.com>
2018-09-21 13:11:42 -0400

    Get excel file loaded into app

--- src/Components/driverTable.jsx
+++ src/Components/driverTable.jsx
@@ -8,26 +8,34 @@ import CONSTANTS from '../constants';
 class DriverTable extends Component {
   constructor() {
     super();
-    this.setState({
+    this.state = {
       trailerSheetJSON: {},
-    });
+    };
+
+    ipcRenderer.on(CONSTANTS.EV_RECEIVE_JSON, this.onReceiveJSON);
   }
 
   componentWillMount() {
     ipcRenderer.send(CONSTANTS.EV_LOAD_FILE);
   }
 
+  componentWillUnmount() {
+    ipcRenderer.removeListener(CONSTANTS.EV_LOAD_FILE, this.onReceiveJSON);
+  }
+
   onReceiveJSON = (_, arg) => {
     this.setState({
       trailerSheetJSON: arg,
     });
+
+    console.log("Received JSON:\n" + arg);
   }
 
   render() {
     const { trailerSheetJSON } = this.state;
 
     return (
-      <p>{trailerSheetJSON}</p>
+      <div><pre>{JSON.stringify(trailerSheetJSON, null, 2) }</pre></div>
     );
   }
 }
